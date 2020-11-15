653f841f7e1683c67d2a60cbd9db035fda23162d
Maciej <neipreis22@gmail.com>
2020-11-15 12:44:38 +0100

    Lab 2 - always remember about 'git add and push' your work when you did it :|

---
+++ NewBrandingStyle.Web/Controllers/AddNewItemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
+
+namespace NewBrandingStyle.Web.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddNewItemController : ControllerBase {
+        public CompanyAddedViewModel Post(CompanyModel Item) {
+            return new CompanyAddedViewModel {
+                NumberOfCharsInName = Item.Name.Length,
+                NumberOfCharsInDescription = Item.Description.Length,
+                IsHidden = !Item.IsVisible,
+            }; ;
+        }
+    }
+}
---
+++ NewBrandingStyle.Web/wwwroot/js/addNewItem.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    const alertElement = document.getElementById("success-alert");
+    const formElement = document.forms[0];
+    const addNewItem = async () => {
+        const requestData = Object.values(formElement).reduce((obj, field) => {
+            if (['Name', 'IsVisible', 'Description'].includes(field.name)) {
+                if (field.name === 'IsVisible') {
+                    if (field.type === 'checkbox') {
+                        obj[field.name] = field.value === 'true';
+                    }
+                } else {
+                    obj[field.name] = field.value;
+                }
+            }
+            return obj;
+        }, {});
+        const response = await fetch('/api/AddNewItem', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        }).then((data) => data);
+        const responseJson = await response.json();
+        if (responseJson) {
+            alertElement.style.display = 'block';
+        }
+    };
+    window.addEventListener("load", () => {
+        formElement.addEventListener("submit", event => {
+            event.preventDefault();
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+})();
