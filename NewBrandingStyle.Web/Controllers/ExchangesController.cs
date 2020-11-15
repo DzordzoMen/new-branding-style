c6ad8a5a5fe4aa09cad11fc0b0153dfd872e2c22
Maciej <neipreis22@gmail.com>
2020-11-15 16:11:28 +0100

    Lab 3 Database

---
+++ NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Database;
+using NewBrandingStyle.Web.Models;
+using NewBrandingStyle.Web.Models.Database;
+
+namespace NewBrandingStyle.Web.Controllers {
+    public class ExchangesController : Controller {
+        private readonly ExchangesDbContext _dbContext;
+
+        public ExchangesController(ExchangesDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+        [HttpPost]
+        public IActionResult Add(CompanyModel item) {
+            var entity = new ItemEntity {
+                Name = item.Name,
+                Description = item.Description,
+                IsVisible = item.IsVisible,
+            };
+
+            _dbContext.Items.Add(entity);
+            _dbContext.SaveChanges();
+
+            return View();
+        }
+    }
+}
--- NewBrandingStyle.Web/Startup.cs
+++ NewBrandingStyle.Web/Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NewBrandingStyle.Web.Database;
 
 namespace NewBrandingStyle.Web
 {
@@ -23,6 +25,11 @@ namespace NewBrandingStyle.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ExchangesDbContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("ExchangeThings")
+                )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
